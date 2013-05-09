e9a702cddfb0f6acc1861de7a1c3d1338df27ffb
Nico Kruithof <Nico@nghk.nl>
2013-05-09 22:35:56 +0200

    I can draw circles

--- src/skin_curve/view/main_view.h
+++ src/skin_curve/view/main_view.h
@@ -5,9 +5,9 @@
 #include <QtGui/QWidget>
 
 #include <boost/shared_ptr.hpp>
-#include <model/model.h>
 
-#include <iostream>
+#include <model/model.h>
+#include <controller/controller.h>
 
 class MainView : public QWidget {
   Q_OBJECT;
@@ -15,29 +15,19 @@ class MainView : public QWidget {
 public:
   MainView(QWidget * parent = 0, Qt::WindowFlags f = 0);
 
-void paintEvent(QPaintEvent *event)
-{
-      QPainter painter(this);
-      
-      //First Circle:
-      //width&height:150px
-      //Beginning(x/y):50px/30px
-      //Each step for Angle parameter is 1/16th of a Degree
-      //so multiply Angle Value with 16
-      painter.drawArc(50,30,150,150,0,16*360);
-      painter.drawArc(200,80,100,100,0,16*360);
-      painter.drawArc(300,130,50,50,0,16*360);
+  virtual void paintEvent(QPaintEvent *event);
 
- QRectF rectangle(10.0, 20.0, 80.0, 60.0);
-
- painter.drawEllipse(rectangle);
-  std::cout << __FUNCTION__ << std::endl;
-    // m_model.update();
-    // m_ui->osg_main_widget->set_scene(m_model.scene());
-}
+  virtual void mouseMoveEvent( QMouseEvent * event );
+  virtual void mousePressEvent( QMouseEvent * event );
+  virtual void mouseReleaseEvent( QMouseEvent * event );
 
 private:
   boost::shared_ptr<Model> m_model;
+  boost::shared_ptr<Controller> m_controller;
+
+private:
+  QPoint m_circle_center;
+  double m_radius;
 };
 
 #endif // VIEW_MAIN_VIEW_H
